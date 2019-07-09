df192bad0a2935169341421fb6a0fb9ae78ac686
Tiago <tiagowebersc@gmail.com>
2019-07-09 15:09:49 +0200

    first page

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Book store</title>
+</head>
+
+<body>
+    <?php require_once header . php ?>
+
+</body>
+
+</html>
